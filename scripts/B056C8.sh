ae8c3721f574cc9c365b2b4e8e7400b66a85ea2f
Daniel Ansorregui <mailszeros@gmail.com>
2026-04-29 19:46:06 +0100

    Add proper script for my use case

--- scripts/B056C8.sh
+++ scripts/B056C8.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-python capture.py --wait 10 --bmp3 https://192.168.0.98:1881/ui $@
+# Proper one
+python capture.py --crop 175,115,320,280 730,240,400,200 1240,125,380,480 \
+--width 1800 --height 1000 --wait 10 --bmp3 https://192.168.0.98:1881/ui $@
+
+# python capture2.py --output out.bmp --capture "url=https://192.168.0.98:1881/ui;x=60;y=100;w=600;h=300;wait=10000;viewport=380x380" --capture "url=https://192.168.0.98:1881/ui;x=60;y=100;w=1200;h=1000;wait=1000;viewport=500x500
